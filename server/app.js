297af1e32d183ae2b131a8538f44d53561fad252
Rohit Bokade <bokaderohit98@gmail.com>
2018-02-24 21:03:05 +0530

    added backend validation to registration form

--- server/app.js
+++ server/app.js
@@ -70,8 +70,6 @@ app.get('/about', (req, res) => {
 });
 
 
-
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
--- server/routes/users.js
+++ server/routes/users.js
@@ -9,4 +9,29 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
+router.post('/register', (req, res) => {
+  var errors = [];
+  if (req.body.password != req.body.password2) {
+    errors.push({text: 'Password do not match'});
+  }
+
+  if (req.body.password.length < 5) {
+    errors.push({text: 'Password must have atleast 5 characters'});
+  }
+
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors,
+      name: req.body.name,
+      email: req.body.email
+    });
+  } else {
+    res.send('passed');
+  }
+});
+
+router.post('/login', (req, res) => {
+  res.send('logged in');
+});
+
 module.exports = router;
